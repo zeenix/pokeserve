4c2d985c9ddf2f19e69ccf6fe46efd353f76775f
Zeeshan Ali <zeeshanak@gnome.org>
2021-03-02 22:42:08 +0100

    Handle requests for pokemon descriptions given a name
    Let's start to get a bit serious and actually handle the HTTP requests
    for pokemon descriptions. Currently, we just return the description from
    pokeapi.co.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
 
 mod poke;
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let response = Response::builder();
+    let uri = req.uri();
+    let path = uri.path();
+
+    let parts: Vec<&str> = path.split("/").into_iter().collect();
+    if parts.len() != 3 || parts[0] != "" || parts[1] != "pokemon" || parts[2] == "" {
+        return Ok(response
+            .status(StatusCode::BAD_REQUEST)
+            .body("Invalid path".into()).unwrap())
+    }
+
+    // FIXME: Very inefficient to create the clients for each request.
+    let desc = poke::Poke::new().fetch_pokemon(parts[2]).await.unwrap();
+
+    Ok(response
+       .status(StatusCode::OK)
+       .body(desc.into()).unwrap())
 }
 
 #[tokio::main]
@@ -14,7 +30,7 @@ async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(hello_world))
+        Ok::<_, Infallible>(service_fn(handle_request))
     });
 
     let server = Server::bind(&addr).serve(make_svc);

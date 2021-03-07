66a9d343f34e5360383d920d3ada61a0f4835e6a
Zeeshan Ali <zeeshanak@gnome.org>
2021-03-07 23:34:28 +0100

    Accept connections from all network interfaces
    Not only the loopback interface.

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Error> {
 
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
     let make_svc = make_service_fn(|_conn| async {
         Ok::<_, Error>(service_fn(handle_request))
